45d2dd1d32237ba041f6589c1c68a5d51b570230
Alex Corkwell <i.am.the.memory@gmail.com>
2018-04-03 13:00:24 -0400

    Add basic argument parsing

--- src/hapcat/hapcat.py
+++ src/hapcat/hapcat.py
@@ -6,13 +6,39 @@
 
 from __future__ import absolute_import, print_function
 
+import argparse
 import sys
+
+import hapcat
 import hapcat.apiserver
 
+
+def make_argparser():
+    """Return an ArgumentParser for hapcatd.
+    """
+
+    parser = argparse.ArgumentParser(
+        description=hapcat.__description__,
+        add_help=True,
+        allow_abbrev=False
+    )
+
+    # Version.
+    parser.add_argument(
+        '-V',
+        '--version',
+        action='version',
+        version='%(prog)s {version}'.format(version=hapcat.__version__)
+    )
+
+    return parser
 
 def main():
     """Start the Hapcat daemon.
     """
+
+    # Parse our arguments.
+    args = make_argparser().parse_args()
 
     print('Initializing Hapcat daemon...')
 
