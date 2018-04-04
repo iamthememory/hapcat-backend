70c3d929f36534fda820ac5dd7a860ded70b3724
Alex Corkwell <i.am.the.memory@gmail.com>
2018-04-04 19:06:35 -0400

    Add GUID model type

---
+++ src/hapcat/types.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Hapcat database model custom types.
+"""
+
+from sqlalchemy.dialects.postgresql import UUID as PostgresUUID
+
+import uuid
+
+from sqlalchemy.types import (
+    CHAR
+    TypeDecorator,
+)
+
+
+class GUID(TypeDecorator):
+    """A platform-independent GUID type.
+
+    This uses PostgreSQL's UUID type, otherwise using CHAR(32), storing
+    as stringified hex values.
+
+    This is taken from the SQLAlchemy examples with minor modifications.
+    """
+
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(PostgresUUID())
+        else:
+            return dialect.type_descriptor(CHAR(32))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == 'postgresql':
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return '%.32x' % uuid.UUID(value).int
+            else:
+                # Hexstring
+                return '%.32x' % value.int
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            if not isinstance(value, uuid.UUID):
+                value = uuid.UUID(value)
+            return value
