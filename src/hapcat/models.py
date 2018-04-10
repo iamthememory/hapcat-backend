d4dbe497fc2fbbc84aba5bf7b0631946c46c2a61
Alex Corkwell <i.am.the.memory@gmail.com>
2018-04-10 17:56:31 -0400

    Add basic locations

--- src/hapcat/models.py
+++ src/hapcat/models.py
@@ -6,12 +6,21 @@
 
 from __future__ import absolute_import, with_statement, print_function
 
-from sqlalchemy.schema import (
-    Column
+from hapcat.types import GUID
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from sqlalchemy.orm import (
+    backref,
+    relationship,
 )
 
-from hapcat.types import GUID
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import (
+    Column,
+    Table,
+    ForeignKey,
+)
 
 from sqlalchemy.types import (
     UnicodeText,
@@ -27,3 +36,24 @@ class Tag(Base):
 
     id = Column(GUID, primary_key=True)
     name = Column(UnicodeText, nullable=False)
+
+class Location(Base):
+    __tablename__ = 'location'
+
+    id = Column(GUID, primary_key=True)
+    name = Column(UnicodeText)
+    address = Column(UnicodeText)
+
+    tags = association_proxy('location_tags', 'tag')
+
+class LocationTag(Base):
+    __tablename__ = 'location_tag'
+    location_id = Column(GUID, ForeignKey('location.id'), primary_key=True)
+    tag_id = Column(GUID, ForeignKey('tag.id'), primary_key=True)
+
+    location = relationship(
+        Location,
+        backref=backref('location_tags', cascade='all, delete-orphan')
+    )
+
+    tag = relationship('Tag')
