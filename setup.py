6beae82264ee147a7ab2137c18372e1c7aa7866b
Alex Corkwell <i.am.the.memory@gmail.com>
2018-04-18 20:36:49 -0400

    Bump version: 0.0.2.dev6 -> 0.0.2.dev7

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ long_desc = '%s\n\n%s' % (readme, changes)
 
 setup(
     name='hapcat',
-    version='0.0.2.dev6',
+    version='0.0.2.dev7',
     description='The backend code for the Hapcat Project',
     long_description=long_desc,
     author=authors,
